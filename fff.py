1f76f5fb837a86cf660ed25f3b27e3cae95994a3
AchPaul <73701254+AchPaul@users.noreply.github.com>
2021-03-22 22:52:52 +0300

    Upd

--- fff.py
+++ fff.py
@@ -5,7 +5,6 @@ def appearance(intervals):
     tp = 0
     for key in intervals:
         if key == 'pupil':
-            flug = True
             for i in intervals[key]:
                 puptime.append(i)
         if key == 'tutor':
@@ -29,10 +28,8 @@ def appearance(intervals):
             tut -= i
         flug = not flug
 
-
     print(pup)
     print(tut)
-
 
 l = {'lesson': [1594663200, 1594666800],
      'pupil': [1594663340, 1594663389, 1594663390, 1594663395, 1594663396, 1594666472],
